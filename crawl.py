fc7779a1bc34ba8efb14bc7f6808fee5c884e303
Zach Tatlock <ztatlock@cs.ucsd.edu>
2011-06-19 19:01:25 -0700

    port crawler from bash to python

---
+++ crawl.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import re
+import sys
+import shlex
+import subprocess
+from BeautifulSoup import BeautifulSoup
+
+ROOT = sys.argv[1]
+
+POST_PATS = [ 'http://sandiego.craigslist.org/.*/[0-9]*.html'
+            , 'http://charlotte.craigslist.org/.*/[0-9]*.html'
+            , 'http://orlando.craigslist.org/.*/[0-9]*.html'
+            ]
+
+def main():
+  # fetch ROOT
+  cmd('wget -O root.html %s' % ROOT)
+
+  # get contents of root.html 
+  f = open('root.html', 'r')
+  html = f.read()
+  f.close()
+
+  posts = extract_posts(html)
+  for post in posts:
+    cmd('wget --force-directories %s' % str(post))
+
+def extract_posts(html):
+  links = extract_links(html)
+  posts = []
+  for link in links:
+    for pat in POST_PATS:
+      if re.match(pat, link):
+        posts.append(link)
+        break
+  return posts
+
+def extract_links(html):
+  soup = BeautifulSoup(html)
+  links = []
+  for tag in soup.findAll('a', href=True):
+    links.append(tag['href'])
+  return links
+
+def cmd(c):
+  r = subprocess.call(shlex.split(c))
+  if r != 0:
+    print 'Warning: wget failed!'
+
+main()
+
+# ---
+
+# Anne's wget program!
+#
+# import sys
+# import urllib
+#
+# url = sys.argv[1]
+#
+# f = urllib.urlopen(url)
+# ls = f.readlines()
+# f.close()
+#
+# for l in ls:
+#     print l,
+#
