fa1008c19e5958284f26fe795ae80e8096350d72
Zach Tatlock <ztatlock@cs.ucsd.edu>
2011-06-28 07:14:53 -0700

    start on simply watchdog to keep clipper running

---
+++ bin/watchdog.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+ROOT=$1
+DATA=$ROOT/data
+CLIP=$ROOT/bin/clipper.py
+
+function running {
+  ps aux \
+    | grep $CLIP \
+    | grep -v grep
+}
+
+function alarm {
+  sendmail ztatlock@gmail.com <<EOF
+To: ztatlock@gmail.com
+From: ztatlock@gmail.com
+Subject: HEADS UP! clipper not running
+
+$(date)
+
+Clipper was not running, so I started it.
+
+EOF
+}
+
+function start {
+  $CLIP --data $DATA \
+        --cities charlotte denver portland tampa minneapolis stlouis \
+        --catgs cas msr m4m m4w w4m w4w \
+        --minWait 50 \
+        --maxWait 60
+}
+
+if ! running; then
+  alarm
+  start
+fi
+
